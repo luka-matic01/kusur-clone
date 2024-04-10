54cf706afe71245c785db8757d58e9dd747f481b
m4t4a <lukamatic129@gmail.com>
2024-04-10 18:44:43 +0200

    Starter pack project

---
+++ app/_layout.js
@@ -0,0 +1,55 @@
+import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
+import { Slot, useRouter, useSegments } from 'expo-router';
+import { useEffect } from 'react';
+import {CLERK_PUBLISHABLE_KEY} from "@env";
+import * as SecureStore from 'expo-secure-store';
+
+
+const InitialLayout = () => {
+  const { isLoaded, isSignedIn } = useAuth();
+  const segments = useSegments();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    const inTabsGroup = segments[0] === '(auth)';
+
+    console.log('User changed: ', isSignedIn);
+
+    if (isSignedIn && !inTabsGroup) {
+      router.replace('/home');
+    } else if (!isSignedIn) {
+      router.replace('/login');
+    }
+  }, [isSignedIn]);
+
+  return <Slot />;
+};
+
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+
+const RootLayout = () => {
+  return (
+    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY} tokenCache={tokenCache}>
+      <InitialLayout />
+    </ClerkProvider>
+  );
+};
+
+export default RootLayout;
---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/register', (req, res) => {
+  // Implement user registration logic here
+});
+
+router.post('/login', (req, res) => {
+  // Implement user login logic here
+});
+
+module.exports = router;
